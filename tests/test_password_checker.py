eda06dcf44c0ccb903dcef40c8c0640442299080
Onuora <onuora.wright@gmail.com>
2023-09-19 17:32:12 +0100

    03 testing for errors Challenge complete on Password Checker

--- tests/test_password_checker.py
+++ tests/test_password_checker.py
@@ -2,4 +2,27 @@ import pytest
 from lib.password_checker import *
 
 def test_if_length_less_than_8():
-    
+    password = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        password.check("cat")
+    error_message = str(e.value)
+    assert error_message == 'Invalid password, must be 8+ characters.'
+
+
+def test_if_length_more_than_8():
+    password = PasswordChecker()
+    result = password.check("catanddog")
+    assert result == True
+
+
+def test_if_password_is_empty_string():
+    password = PasswordChecker()
+    with pytest.raises(Exception) as e:
+        password.check("")
+    error_message = str(e.value)
+    assert error_message == 'Invalid password, must be 8+ characters.'
+
+def test_if_length_is_equal_to_8():
+    password = PasswordChecker()
+    result = password.check("12345678")
+    assert result == True
--- tests/test_present.py
+++ tests/test_present.py
@@ -8,3 +8,12 @@ def test_if_contents_is_not_none():
         present.wrap("toy")
     error_message = str(e.value)
     assert error_message == "A contents has already been wrapped."
+
+
+def test_if_contents_is_none():
+    present = Present()
+    with pytest.raises(Exception) as e:
+        present.unwrap()
+    error_message = str(e.value)
+    assert error_message == "No contents have been wrapped."
+    
